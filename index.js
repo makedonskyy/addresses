55907230191067e7e579d37a45ef01bdab4378a0
Yaroslava Makedonskaya <yaroslavamakedonskaya@yandex.ru>
2022-12-12 14:43:45 +0300

    did it better

--- index.js
+++ index.js
@@ -5,22 +5,27 @@ let btnString = document.getElementById('string-btn')
 let btnBlock = document.getElementById('block-btn')
 
 btnString.addEventListener('click', function () {
-if (!block.classList.contains('d-none')) {
-    block.classList.add('d-none')
-    string.classList.remove('d-none')
-}
-btnString.style.fill =  '#AA0000'
-}
-)
+    if (!block.classList.contains('d-none')) {
+        block.classList.add('d-none')
+        string.classList.remove('d-none')
+        if (btnString.classList.contains('grey-btn')) {
+            btnString.classList.remove('grey-btn')
+            btnString.classList.add('active-btn')
+        }
+    }
+    btnBlock.classList.remove('active-btn')
+    btnBlock.classList.add('grey-btn')
+})
 
 btnBlock.addEventListener('click', function () {
     if (!string.classList.contains('d-none')) {
         string.classList.add('d-none')
         block.classList.remove('d-none')
+        if (btnBlock.classList.contains('grey-btn')) {
+            btnBlock.classList.remove('grey-btn')
+            btnBlock.classList.add('active-btn')
+        }
     }
-    btnBlock.style.fill =  '#AA0000'
-    }
-    )
-
-console.log(btnString);
-console.log(string);
+    btnString.classList.remove('active-btn')
+    btnString.classList.add('grey-btn')
+})
